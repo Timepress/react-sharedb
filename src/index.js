7a4032986e0cbe0e8e75884133a2bf98b5a1daf7
Jan "siOnzee" Tržický <jan@sionzee.cz>
2018-07-17 12:33:16 +0200

    Removed default initialization of GlobalObservables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './initGlobalObservables'
+import initGlobalObservablesFnc from './initGlobalObservables'
 import subscribe, { batching } from './subscribe'
 export { default as model } from '@react-sharedb/model'
 export { subscribe }
 export const batchModel = batching.batch.bind(batching)
 export { default as _semaphore } from './semaphore'
+export { initGlobalObservablesFnc }
