6f58b77c8d19d4b9dd086bf4c324c90465e1bae6
Jan "siOnzee" Tržický <jan@sionzee.cz>
2018-07-17 14:36:28 +0200

    Removed transform-classes

--- test/_client/index.js
+++ test/_client/index.js
@@ -1,8 +1,8 @@
 // Make it behave like a browser to trick Racer
 // https://github.com/derbyjs/racer/blob/master/lib/util.js#L3
 process.title = 'browser'
-import './setupJsDom'
-import './setupRacerHighway'
+import setupJsDom from './setupJsDom'
+import setupRacerHighway from './setupRacerHighway'
 import ws from 'ws'
 
 global.WebSocket = ws
